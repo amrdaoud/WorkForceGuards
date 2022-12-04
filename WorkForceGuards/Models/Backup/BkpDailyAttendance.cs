c10d7d72caa35f114006fea1d1b65006e9c68b2e
amrdaoud <amr.f.daoud@gmail.com>
2022-12-04 10:13:06 +0300

    Add Backup Folder

---
+++ WorkForceGuards/Models/Backup/BkpDailyAttendance.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace WorkForceManagementV0.Models.Backup
+{
+    public class BkpDailyAttendance
+    {
+        public BkpDailyAttendance()
+        {
+
+        }
+        public BkpDailyAttendance(DailyAttendance model, string alias)
+        {
+            var timezone = TimeZoneInfo.FindSystemTimeZoneById("Arab Standard Time");
+            ActionTime = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, timezone);
+            Id = 0;
+            DailyAttendanceId = model.Id;
+            Alias = alias;
+            StaffMemberId = model.StaffMemberId;
+            ScheduleId = model.ScheduleId;
+            Day = model.Day;
+            AttendanceTypeId = model.AttendanceTypeId;
+            TransportationRouteId = model.TransportationRouteId;
+            HeadOfSectionId =   model.HeadOfSectionId;
+            IsDeleted = false;
+        }
+        [Key]
+        public int Id { get; set; }
+        public int DailyAttendanceId { get; set; }
+        public string Alias { get; set; }
+        public DateTime ActionTime { get;set; }
+
+        public int StaffMemberId { get; set; }
+
+
+        public int ScheduleId { get; set; }
+        public DateTime Day { get; set; }
+
+        [ForeignKey("AttendanceType")]
+        public int AttendanceTypeId { get; set; }
+
+
+        public int? TransportationRouteId { get; set; }
+
+        public int HeadOfSectionId { get; set; }
+        public virtual AttendanceType AttendanceType { get; set; }
+
+
+
+
+
+
+
+
+
+        public bool IsDeleted { get; set; }
+
+        public virtual ICollection<BkpScheduleDetail> ScheduleDetails { get; set; }
+    }
+}
---
+++ WorkForceGuards/Models/Backup/BkpScheduleDetail.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WorkForceManagementV0.Models.Backup
+{
+    public class BkpScheduleDetail
+    {
+        public BkpScheduleDetail()
+        {
+
+        }
+        public BkpScheduleDetail(ScheduleDetail model)
+        {
+            Id = 0;
+            DailyAttendanceId = model.DailyAttendanceId;
+            ActivityId = model.ActivityId;
+            IntervalId = model.IntervalId;
+            Duration = model.Duration;
+            ScheduleId = model.ScheduleId;
+        }
+        [Key]
+        public int Id { get; set; }
+        [ForeignKey("DailyAttendance")]
+        public int DailyAttendanceId { get; set; }
+        public virtual BkpDailyAttendance DailyAttendance { get; set; }
+        [ForeignKey("Activity")]
+        public int ActivityId { get; set; }
+
+        [ForeignKey("Interval")]
+        public int IntervalId { get; set; }
+
+        public double? Duration { get; set; }
+        public int ScheduleId { get; set; }
+
+        public virtual Interval Interval { get; set; }
+        public virtual Activity Activity { get; set; }
+    }
+}
